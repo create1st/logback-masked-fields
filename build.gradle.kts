f4ec39541fb6dec138dd4516959a98f4d180b574
Sebastian Gil <sebastian.gil@gmail.com>
2021-04-30 01:01:30 +1000

    Maven publish v1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,8 +98,8 @@ publishing {
     repositories {
         maven {
             // change URLs to point to your repos, e.g. http://my.org/repo
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/releases")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
             url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
             credentials {
                 username = findProperty("ossrhUsername").toString()
