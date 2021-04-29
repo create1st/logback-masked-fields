9abab265574c01c4637263aeaf4f7eb9c156b27b
Sebastian Gil <sebastian.gil@gmail.com>
2021-04-30 00:50:16 +1000

    Maven central release v1.3.0 (#5)
    * Maven publish v1.3.0
    
    * Maven publish v1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("io.codearte.nexus-staging")  version "0.30.0"
-    kotlin("jvm") version "1.5.0"
+    kotlin("jvm") version "1.4.32"
+    signing
+    `maven-publish`
 }
 
 group = "com.craftandtechnology"
-version = "0.0.1-SNAPSHOT"
+version = "1.3.0"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -43,4 +44,71 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "logback-masked-fields"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("logback-masked-fields")
+                description.set("Logback layout to obscure confidential or secure information")
+                url.set("https://github.com/create1st/logback-masked-fields")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("create1st")
+                        name.set("Sebastian Gil")
+                        email.set("sebastian.gil@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("https://github.com/create1st/logback-masked-fields.git")
+                    developerConnection.set("git@github.com:create1st/logback-masked-fields.git")
+                    url.set("https://github.com/create1st/logback-masked-fields")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = findProperty("ossrhUsername").toString()
+                password = findProperty("ossrhPassword").toString()
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
